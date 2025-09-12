0cac75b5319a8f53dcff48cf17b4c3a7c8954b48
Mathis Langella <matislangella@gmail.com>
2025-09-12 16:41:05 +0100

    correctiondebug

--- src/main.go
+++ src/main.go
@@ -1,18 +1,13 @@
-struct Character {
+type Character struct{
 	Nom string,
 	Classe string,
 	LVL int,
 	PVMax int,
 	PVActuels int,
-	Inventaire []string,
+	Inventaire [][]string,
 }
 
 func initCharacter(nom string, classe string) Character {
-		Nom := nom
-		Classe := classe
-		LVL := 1
-		PVMax := 100
-		PVActuels := PVMax
-		Inventaire := []string
+	return var Character = Character{nom, Classe, 1, 100, 100, [[3,"Potion de Vie"]]}
 }
 	
